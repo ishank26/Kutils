b511c7e4528375520403f32acab8158ef1decd6e
Ishank sharma <ishank269@gmail.com>
2016-09-25 14:46:04 +0530

    updated

--- param_op/sci_hyerop.py
+++ param_op/sci_hyerop.py
@@ -33,25 +33,7 @@ init=["normal", "uniform"]
 dropout=dict(dropout=dropout)
 print dropout
 ############################# Begin model ###################
-'''
-# load pima indians dataset
-dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
-# create model
-model = KerasClassifier(build_fn=create_model)
-# grid search epochs, batch size and optimizer
-optimizers = ['rmsprop', 'adam']
-init = ['glorot_uniform', 'normal', 'uniform']
-epochs = numpy.array([50, 100, 150])
-batches = numpy.array([5, 10, 20])
-param_grid = dict(optimizer=optimizers, nb_epoch=epochs, batch_size=batches, init=init)
-grid = GridSearchCV(estimator=model, param_grid=param_grid)
-grid_result = grid.fit(X, Y)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-'''
+
 def my_model(dropout):
     ############ model params ################
     line_length = 248 # seq size
